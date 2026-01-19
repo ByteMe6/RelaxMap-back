985ab0831da9cc41d402709b8a7cbcd697afd514
Maksym-Ryaboshapka <testbs777888@gmail.com>
2026-01-19 08:14:03 +0200

    init maven project

---
+++ src/main/java/org/example/relaxmapback/RelaxMapBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.relaxmapback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxMapBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RelaxMapBackApplication.class, args);
+  }
+
+}
